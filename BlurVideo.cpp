2f8e4017500e6f91a482b2da3148f85f82118034
DavidVuescu <davidvuescu@gmail.com>
2023-07-04 04:37:01 +0300

    Updated BlurVideo file generation Now every temporary file is randomly generated

--- BlurVideo.cpp
+++ BlurVideo.cpp
@@ -2,9 +2,35 @@
 #include <stdlib.h>
 #include <opencv2/opencv.hpp>
 #include <unistd.h>
+#include <random>
+#include <string>
 
-// Function to blur the faces in a video using Haar cascade
-void blur_video(const char* inputFilename, const char* outputFilename, const char* output_path, const char* option)
+char* generate_random(bool isWAVFile) {
+    const char CHARACTERS[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    std::uniform_int_distribution<> distribution(0, sizeof(CHARACTERS) - 2); // subtract 2 to account for the null character
+
+    int length = isWAVFile ? 14 : 15; // Allocate length according to file type
+    char* random_string = new char[length];  // length includes space for extension and null character
+
+    for (int i = 0; i < 10; ++i) {
+        random_string[i] = CHARACTERS[distribution(generator)];
+    }
+
+    // Append file extension based on isWAVFile flag
+    if(isWAVFile) {
+        strcpy(random_string + 10, ".wav");
+    } else {
+        strcpy(random_string + 10, ".mp4");
+    }
+
+    return random_string;
+}
+
+
+void blur_video(const char *inputFilename, const char *outputFilename, const char *output_path, const char *option)
 {
     cv::CascadeClassifier cascade;
     cascade.load("haarcascade_frontalface_default.xml"); // Load the Haar cascade XML file
@@ -16,7 +42,7 @@ void blur_video(const char* inputFilename, const char* outputFilename, const cha
         return;
     }
 
-    cv::VideoWriter writer(outputFilename, cv::VideoWriter::fourcc('X', 'V', 'I', 'D'), capture.get(cv::CAP_PROP_FPS),
+    cv::VideoWriter writer(outputFilename, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), capture.get(cv::CAP_PROP_FPS),
                            cv::Size(capture.get(cv::CAP_PROP_FRAME_WIDTH), capture.get(cv::CAP_PROP_FRAME_HEIGHT)));
 
     cv::Mat frame, grayFrame;
@@ -29,7 +55,7 @@ void blur_video(const char* inputFilename, const char* outputFilename, const cha
         cascade.detectMultiScale(grayFrame, faces, 1.1, 3, 0, cv::Size(30, 30));
 
         // Blur the detected faces
-        for (const cv::Rect& face : faces)
+        for (const cv::Rect &face : faces)
         {
             cv::Mat faceROI = frame(face);
             cv::GaussianBlur(faceROI, faceROI, cv::Size(55, 55), 0);
@@ -41,42 +67,38 @@ void blur_video(const char* inputFilename, const char* outputFilename, const cha
     capture.release();
     writer.release();
 
-    if (strcmp("1", option) == 0)
+    if (strcmp(option, "1") == 0)
     {
-        // Output audio file path
-        const char* audioFilePath = "output_audio.mp3";
+        //const char *audioFilePath = "output_audio.wav";
+        const char *audioFilePath = generate_random(true);
 
         // Build the FFmpeg command
         char command[1024];
-        sprintf(command, "ffmpeg -i %s -vn -acodec copy %s > /dev/null 2>&1", inputFilename, audioFilePath);
+        sprintf(command, "ffmpeg -i %s -vn -acodec pcm_s16le -ar 44100 -ac 2 %s  > /dev/null 2>&1", inputFilename, audioFilePath);
         system(command);
 
         char combineCommand[1024];
-        sprintf(combineCommand, "ffmpeg -i %s -i %s -c:v copy -c:a copy %s > /dev/null 2>&1", outputFilename, audioFilePath, output_path);
+        sprintf(combineCommand, "ffmpeg -i %s -i %s -c:v copy -c:a aac -strict experimental %s > /dev/null 2>&1", outputFilename, audioFilePath, output_path);
         system(combineCommand);
 
-        std::rename(outputFilename, ("%s.mp4", output_path));
-
         remove(audioFilePath);
+        delete[] audioFilePath;
     }
-    
 }
 
-// Function to distort the audio in a video using FFmpeg
-void audio_distort(const char* inputFilename, const char* distortedAudio, const char* output_path, const char* option)
+void audio_distort(const char *inputFilename, const char *distortedAudio, const char *output_path, const char *option)
 {
     char command[512];
     sprintf(command, "ffmpeg -i %s -af \"asetrate=44100*1.5, atempo=0.6667\" -c:v copy %s > /dev/null 2>&1", inputFilename, distortedAudio);
     system(command);
 
-    if (strcmp("2", option) == 0)
+    if (strcmp(option, "2") == 0)
     {
         std::rename(distortedAudio, ("%s.mp4", output_path));
     }
 }
 
-// Function to combine the blurred video and distorted audio into a single video using FFmpeg
-void combine_videos(const char* blurredVideo, const char* distortedAudio, const char* output_path)
+void combine_videos(const char *blurredVideo, const char *distortedAudio, const char *output_path)
 {
     // Use FFmpeg to combine the videos
     char command[512];
@@ -84,54 +106,34 @@ void combine_videos(const char* blurredVideo, const char* distortedAudio, const 
     system(command);
 }
 
-void finalize(const char* inputVideo, const char* blurredVideo, const char* output_path)
+int main(int argc, char *argv[])
 {
-    // Modify audio pitch
-    char cmd_audio[512];
-    sprintf(cmd_audio, "ffmpeg -i %s -filter_complex '[0:a]atempo=0.7,asetrate=44100*1.3[out]' -map '[out]' %s_SOUND_MODIFIED.wav -y > > /dev/null 2>&1", inputVideo, output_path);
-    system(cmd_audio);
-
-    // Merge video and modified audio
-    char cmd[512];
-    sprintf(cmd, "ffmpeg -i %s -i %s_SOUND_MODIFIED.wav -c:v copy -c:a aac -strict experimental %s -y > > /dev/null 2>&1", blurredVideo, output_path, output_path);
-    system(cmd);
-
-    // Clean up temporary files
-    // remove((std::string(output_path) + "_SOUND_MODIFIED.wav").c_str());
-    // remove((std::string(output_path) + "_SOUND.wav").c_str());
-
-    printf("VIDEO READY\n");
-}
-
-int main(int argc, char* argv[])
-{
-    if (argc < 3)
-    {		
+    if (argc < 4)
+    {
         printf("Usage: program_name option input_video output_video\n");
         return 1;
     }
 
-    const char* option = argv[1];
-    const char* inputVideo = argv[2];
-    const char* blurredVideo = "blurred_video.mp4";
-    const char* distortedAudio = "distorted_audio.mp4";
+    const char *option = argv[1];
+    const char *inputVideo = argv[2];
+    //const char *blurredVideo = "blurred_video.mp4";
+    const char *blurredVideo = generate_random(false);
+    //const char *distortedAudio = "distorted_audio.mp4";
+    const char *distortedAudio = generate_random(false);
     std::string output_path = argv[3];
 
     if (strcmp(option, "1") == 0)
     {
-        // Blur video
         blur_video(inputVideo, blurredVideo, output_path.c_str(), option);
     }
 
     if (strcmp(option, "2") == 0)
     {
-        // Distort audio
         audio_distort(inputVideo, distortedAudio, output_path.c_str(), option);
     }
 
     if (strcmp(option, "3") == 0)
     {
-        // Combine videos
         blur_video(inputVideo, blurredVideo, output_path.c_str(), option);
         audio_distort(inputVideo, distortedAudio, output_path.c_str(), option);
         combine_videos(blurredVideo, distortedAudio, output_path.c_str());
@@ -143,6 +145,9 @@ int main(int argc, char* argv[])
     // Clean up intermediate files if needed
     remove(blurredVideo);
     remove(distortedAudio);
+
+    delete[] blurredVideo;
+    delete[] distortedAudio;
 
     return 0;
 }
